8c2312315fa2593e7cbf1ea25e87e879253fb48c
Renato Golin <rengolin@systemcall.eu>
2026-01-12 12:13:32 +0000

    [Ingress] Add pytorch hub importer

---
+++ examples/ingress/torch/hub_load.py
@@ -0,0 +1,45 @@
+# RUN: %PYTHON %s
+
+"""
+Example demonstrating how to load a PyTorch Hub model to MLIR using Lighthouse
+without initializing the model class on the user's side.
+
+The script loads the model from the hub, then uses 'lighthouse.ingress.torch.import_from_model'.
+"""
+
+import argparse
+import torch
+from lighthouse.ingress.torch import import_from_model
+from mlir import ir
+
+# https://docs.pytorch.org/docs/stable/hub.html#torch.hub.load
+def load_with_rand(repo: str, model: str, pretrained: bool = True):
+  model = torch.hub.load(repo, model, pretrained=pretrained, trust_repo="check")
+  input_shape = model.parameters()["input"].shape
+  sample_input = torch.randn(*input_shape)
+  ir_context = ir.Context()
+  return import_from_model(model, sample_args=(sample_input,), ir_context=ir_context)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        help="Model name (e.g., 'pytorch/vision/resnet50')",
+    )
+    args = parser.parse_args()
+
+    if args.model is None:
+      models = ["pytorch/vision/resnet18",
+                "huggingface/google-bert/bert-base-german-cased"]
+    else:
+      models = [args.model]
+
+    for model_name in models:
+      print(f"Loading model from hub: {model_name}")
+      mlir_module_ir: ir.Module = load_with_rand(model_name.split('/')[0], model_name.split('/')[1])
+      print(f"Loaded MLIR module for model: {model_name}")
+      print("\n\nModule dump:")
+      mlir_module_ir.dump()
+
